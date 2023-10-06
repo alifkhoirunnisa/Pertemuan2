9d5de9e9ba0b3dcc61cc927a799ce3ec6c759ff1
alifkhoirunnisa <145516863+alifkhoirunnisa@users.noreply.github.com>
2023-10-06 10:49:29 +0700

    Add files via upload

---
+++ alif/MasterMobil.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan2.alif;
+
+/**
+ *
+ * @author X1
+ */
+public class MasterMobil {
+    
+     String warnamobil ;
+    String merkmobil ;
+    int tahunmobil ;
+    
+    }
+ 
---
+++ alif/Mobil.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan2.alif;
+
+/**
+ *
+ * @author X1
+ */
+public class Mobil {
+     public static void main(String[] args) {
+        MasterMobil honda = new MasterMobil();
+       
+    System.out.println("Ini adalah mobil honda");
+    System.out.println("Warna honda : " + honda.warnamobil);
+    System.out.println("Merek honda : "+ honda.merkmobil);
+    System.out.println("Tahun Pembuatan: "+ honda.tahunmobil);            
+    }
+             
+  }
